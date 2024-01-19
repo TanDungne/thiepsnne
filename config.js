9340cad1d6519e71e66f8577935955213ffc9b7b
TanDungne <154716293+TanDungne@users.noreply.github.com>
2024-01-19 23:36:59 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Happy Birthday';
+const titleCard = 'Gửi em yêu';
+const contentCard = 'Chúc em yêu của anh Sinh nhật tràn ngập niềm vui và những nụ cười. Mong quý cô của anh mãi xinh đẹp và hạnh phúc. Mãi yêu em!';
+
+const giftImage = 'emyeu.jpg';
+
---
+++ present.js
@@ -0,0 +1,62 @@
+
+
+var to = nameGirl;
+var gift_url = giftUrl;
+var gift_image_url = giftImage || giftImageBase64;
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+function playMusic(){
+  var myAudio = document.getElementById('playAudio');
+  if (myAudio.duration > 0 && !myAudio.paused) {
+      //Its playing...do your job
+
+  } else {
+    myAudio.play()
+    //Not playing...maybe paused, stopped or never played.
+
+  }
+}
+
+function init() {
+  var graphElem = document.querySelector('.present-box > .side.top .to');
+  graphElem.setAttribute('data-before', eventName);
+  document.querySelector('#card .title-card').innerHTML = `💘${titleCard}💘`;
+  document.querySelector('#card .content-card').innerHTML = `${contentCard}`;
+
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById('card').classList.add('card-show');
+    playMusic();
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
+
+
